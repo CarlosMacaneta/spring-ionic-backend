3fa999715cbfabb4750d7d5c035a76f4e066accb
Carlos Macaneta <carlosmacaneta@hotmail.com>
2021-05-17 15:52:18 +0200

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/webapplication/demo/repositories/ClienteRepository.java
+++ src/main/java/com/webapplication/demo/repositories/ClienteRepository.java
@@ -3,6 +3,7 @@ package com.webapplication.demo.repositories;
 import com.webapplication.demo.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -11,4 +12,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
     
+    @Transactional(readOnly = true)
+    public Cliente findByEmail(String email);
 }
--- src/main/java/com/webapplication/demo/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/webapplication/demo/services/validation/ClienteInsertValidator.java
@@ -1,14 +1,17 @@
 
 package com.webapplication.demo.services.validation;
 
+import com.webapplication.demo.domain.Cliente;
 import com.webapplication.demo.domain.enums.TipoCliente;
 import com.webapplication.demo.dto.ClienteNewDTO;
+import com.webapplication.demo.repositories.ClienteRepository;
 import com.webapplication.demo.resources.exceptions.FieldMessage;
 import com.webapplication.demo.services.validation.utils.BR;
 import java.util.ArrayList;
 import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
@@ -16,6 +19,9 @@ import javax.validation.ConstraintValidatorContext;
  */
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+    
     @Override
     public void initialize(ClienteInsert constraintAnnotation) {
     }
@@ -30,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         
         if (clienteDTO.getTipoCliente().equals(TipoCliente.PESSOA_JURIDICA.getCodigo()) && !BR.isValidCNPJ(clienteDTO.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ é inválido"));
+        }
+        
+        Cliente aux = clienteRepository.findByEmail(clienteDTO.getEmail());
+        if (aux != null) {
+            list.add(new FieldMessage("email", "O email inserido já existe"));
         }
         
         list.forEach(e -> {
